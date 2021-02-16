60eb2bb1b46b8eecb53a0b1b8c7edaba7a7d12c2
Michel França <fr.mich17@gmail.com>
2021-02-16 03:10:34 -0300

    Hello world feito em Java

---
+++ src/br/com/immichjs/Main.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package br.com.immichjs;
+
+/**
+ * @author Michel França
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world in Java :)");
+	}
+
+}
